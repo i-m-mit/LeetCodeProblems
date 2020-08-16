b6cce9c67ccbca1f2a37c9c24704e79c7fd46f02
Mit Shah <shahmit@outlook.com>
2020-08-16 12:28:26 +0530

    Partition Equal Subset Sum(https://bit.ly/3g5QIxj)

---
+++ DynamicProgramming/PartitionEqualsSubsetSum.java
@@ -0,0 +1,38 @@
+package DynamicProgramming;
+
+import Util.InputHelper;
+
+public class PartitionEqualsSubsetSum {
+    public boolean canPartition(int[] nums) {
+        // check edge case
+        if (nums == null || nums.length == 0) {
+            return true;
+        }
+        // preprocess
+        int volumn = 0;
+        for (int num : nums) {
+            volumn += num;
+        }
+        if (volumn % 2 != 0) {
+            return false;
+        }
+        volumn /= 2;
+        // dp def
+        boolean[] dp = new boolean[volumn + 1];
+        // dp init
+        dp[0] = true;
+        // dp transition
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = volumn; j >= nums[i]; j--) {
+                dp[j] = dp[j] || dp[j - nums[i]];
+            }
+        }
+        return dp[volumn];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = InputHelper.stringToIntegers(args);
+        PartitionEqualsSubsetSum o = new PartitionEqualsSubsetSum();
+        System.out.println(o.canPartition(nums));
+    }
+}
