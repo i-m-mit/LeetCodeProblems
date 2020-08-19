13e281b6cf2cf1c24afeff8c65ff88b27a323b96
Mit Shah <shahmit@outlook.com>
2020-08-19 15:25:03 +0530

    First Unique Character in a String(https://bit.ly/3g7HG3b)

---
+++ StringProblems/FirstUniqueCharacterInString.java
@@ -0,0 +1,25 @@
+package StringProblems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueCharacterInString {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FirstUniqueCharacterInString o = new FirstUniqueCharacterInString();
+
+        System.out.println(o.firstUniqChar("loveleetcode"));
+    }
+}
