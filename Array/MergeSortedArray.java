bd678044764790cbcce5342e839879f57c410474
Mit Shah <shahmit@outlook.com>
2021-03-28 10:28:52 +0530

    Merge Sorted Array with List

--- Array/MergeSortedArray.java
+++ Array/MergeSortedArray.java
@@ -1,5 +1,6 @@
 package Array;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import Util.InputHelper;
@@ -18,6 +19,22 @@ public class MergeSortedArray {
         }
         while (j >= 0) {
             nums1[k--] = nums2[j--];
+        }
+    }
+
+    public void merge(ArrayList<Integer> a, ArrayList<Integer> b) {
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        while (i >= 0 && j >= 0) {
+            if (a.get(i) <= b.get(j)) {
+                a.add(i + 1, b.get(j));
+                j--;
+            } else {
+                i--;
+            }
+        }
+        while (j >= 0) {
+            a.add(0, b.get(j--));
         }
     }
 
